f0c81f48038e3328291c11fc6a3d9fcac34e2750
Fabxo99 <fabricioarbaiza99@gmail.com>
2025-05-23 21:04:19 -0500

    version 2

---
+++ bucles.py
@@ -0,0 +1,11 @@
+print("ejemplos de blucles")
+
+
+datos = ["12", "abc", "45", None, "99"]
+
+for dato in datos:
+    try:
+        numero = int(dato)
+        print(f"Convertido: {numero}")
+    except (ValueError,TypeError):
+        print(f"Error al convertir: {dato}")
